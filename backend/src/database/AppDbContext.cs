b9b497b81accd6a9c6723dd07da4bf0813839972
Carlos Manuel González <cmglezpdev@gmail.com>
2023-09-22 11:53:10 -0400

    feat: update DbContext

--- backend/src/database/AppDbContext.cs
+++ backend/src/database/AppDbContext.cs
@@ -1,3 +1,4 @@
+using backend.localization;
 using backend.users;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,4 +11,11 @@ public class AppDbContext : DbContext
   public DbSet<User> Users { get; set; }
   public DbSet<Province> Provinces { get; set; }
   public DbSet<Municipality> Municipalities { get; set; }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
+    modelBuilder.Entity<PlatformModel>().Property(p => p.CreatedAt).HasDefaultValueSql("NOW()");
+    modelBuilder.Entity<PlatformModel>().Property(p => p.UpdatedAt).HasDefaultValueSql("NOW()").ValueGeneratedOnAddOrUpdate();
+  }
 }

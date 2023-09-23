2407d364fdd7bce4f30ff13659383c5f2a797a49
Carlos Manuel González <cmglezpdev@gmail.com>
2023-09-23 18:41:59 -0400

    fix: cycles relationships

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using backend.database;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,7 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

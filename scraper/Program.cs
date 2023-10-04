81f2e27834417a4a86365961b7dab9fb901f0c12
Jorge Aspiolea <aspiolea28@gmail.com>
2023-10-04 01:16:16 -0400

    add scraper branch

---
+++ scraper/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace Scraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = "https://db.ygoprodeck.com/api/v7/cardinfo.php";
+
+                client.DefaultRequestHeaders.Clear();
+
+                var response = client.GetAsync(url).Result;
+
+                var res = response.Content.ReadAsStringAsync().Result;
+                dynamic r = JObject.Parse(res);
+
+                System.Console.WriteLine(r);
+            }
+        }
+    }
+}

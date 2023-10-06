e6c069f0bfcf177cfa8242df21b517488924e671
Carlos Manuel González <cmglezpdev@gmail.com>
2023-10-06 13:47:37 -0400

    chore: add frontend base project

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Counter />
+</template>
+
+<script lang="ts">
+  import Counter from './components/Counter.vue'
+
+  export default {
+    components: {
+      Counter
+    }
+  }
+</script>

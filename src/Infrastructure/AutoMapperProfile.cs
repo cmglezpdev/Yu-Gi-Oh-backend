0c905278b9a9ccd983fb659d90552c5c2c4ef275
Carlos Manuel González <cmglezpdev@gmail.com>
2023-10-21 15:17:37 -0400

    feat(infrastructure/mapper): add mapping from Entities to Outputs Dtos

--- src/Infrastructure/AutoMapperProfile.cs
+++ src/Infrastructure/AutoMapperProfile.cs
@@ -6,7 +6,14 @@ public class AutoMapperProfiles : Profile
 {
   public AutoMapperProfiles()
   {
-    CreateMap<MunicipalityCreationDto, Municipality>();
-    CreateMap<ProvinceCreationDto, Province>();
+    CreateMap<Municipality, MunicipalityOutputDto>()
+      .ForMember(dto => dto.Id, ent => ent.MapFrom(src => src.Id))
+      .ForMember(dto => dto.Name, ent => ent.MapFrom(src => src.Name))
+      .ForMember(dto => dto.ProvinceId, ent => ent.MapFrom(src => src.Province.Id));
+
+    CreateMap<Province, ProvinceOutputDto>()
+      .ForMember(dto => dto.Id, ent => ent.MapFrom(src => src.Id))
+      .ForMember(dto => dto.Name, ent => ent.MapFrom(src => src.Name))
+      .ForMember(dto => dto.Municipalities, ent => ent.MapFrom(src => src.Municipalities));
   }
 }

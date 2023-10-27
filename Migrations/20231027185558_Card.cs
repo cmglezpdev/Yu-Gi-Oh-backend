dfbac02079ac31951154b84a828b1f3bcba05ab8
aspio28 <aspiolea28@gmail.com>
2023-10-27 16:19:32 -0400

    add card migrations

---
+++ Migrations/20231027185558_Card.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Card : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Archetypes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "varchar(100)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "NOW()"),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "NOW()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Archetypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "cards",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Type = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Desc = table.Column<string>(type: "text", nullable: false),
+                    ImageUrl = table.Column<string>(type: "varchar(255)", nullable: false),
+                    ImageUrlSmall = table.Column<string>(type: "varchar(255)", nullable: false),
+                    ImageUrlCropped = table.Column<string>(type: "varchar(255)", nullable: false),
+                    ArchetypeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Discriminator = table.Column<string>(type: "text", nullable: false),
+                    Rice = table.Column<string>(type: "varchar(100)", nullable: true),
+                    Level = table.Column<int>(type: "integer", nullable: true),
+                    Atk = table.Column<int>(type: "integer", nullable: true),
+                    Def = table.Column<int>(type: "integer", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "NOW()"),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "NOW()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_cards_Archetypes_ArchetypeId",
+                        column: x => x.ArchetypeId,
+                        principalTable: "Archetypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cards_ArchetypeId",
+                table: "cards",
+                column: "ArchetypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cards");
+
+            migrationBuilder.DropTable(
+                name: "Archetypes");
+        }
+    }
+}

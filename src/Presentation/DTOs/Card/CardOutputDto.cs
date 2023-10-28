61b64b6249b1747b1c97faa88fcf1dcad137365e
aspio28 <aspiolea28@gmail.com>
2023-10-28 17:43:18 -0400

    add CardOutputDto

--- src/Presentation/DTOs/Card/CardOutputDto.cs
+++ src/Presentation/DTOs/Card/CardOutputDto.cs
@@ -1,6 +1,11 @@
 namespace backend.Presentation.DTOs;
 
+#pragma warning disable CS8618
 public class CardOutputDto
 {
-    
+    public Guid id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Desc  { get; set; }
+    public Guid ArchetypeId { get; set; }
 }

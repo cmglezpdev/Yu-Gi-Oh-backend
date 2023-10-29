bcef7cfb2074e63086bf99e2b0bc44b0d704716e
aspio28 <aspiolea28@gmail.com>
2023-10-28 21:39:41 -0400

    add ArchetypeController

--- src/Presentation/Controllers/ArchetypeController.cs
+++ src/Presentation/Controllers/ArchetypeController.cs
@@ -1,3 +1,7 @@
+using AutoMapper;
+using backend.Application.Services;
+using backend.Infrastructure.Entities;
+using backend.Presentation.DTOs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Presentation.Controllers;
@@ -6,5 +10,17 @@ namespace backend.Presentation.Controllers;
 [Route("api/[controller]")]
 public class ArchetypeController : ControllerBase
 {
-
+    private readonly ArchetypeService _service;
+    private readonly IMapper _mapper;
+    public ArchetypeController(ArchetypeService service, IMapper mapper)
+    {
+        _service = service;
+        _mapper = mapper;
+    }
+    [HttpGet("{id:Guid}")]
+    public async Task<ActionResult<Archetype>> GetById(Guid Id)
+    {
+        var municipality = await _service.GetArchetypeByIdAsync(Id);
+        return Ok(_mapper.Map<ArchetypeOutputDto>(municipality));
+    }
 }
---
+++ src/Presentation/DTOs/Archetype/ArchetypeOutputDto.cs
@@ -0,0 +1,8 @@
+namespace backend.Presentation.DTOs;
+
+#pragma warning disable CS8618
+public class ArchetypeOutputDto
+{
+    public Guid id { get; set; }
+    public string Name { get; set; }
+}

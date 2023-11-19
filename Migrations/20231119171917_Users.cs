3a5b93a89ad040de1e3b9d00905184796a3961bf
Carlos Manuel González <cmglezpdev@gmail.com>
2023-11-19 15:58:41 -0500

    chore(migrations)[]: add users changes to the database

---
+++ Migrations/20231119171917_Users.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Users : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "users",
+                type: "varchar(100)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_UserName",
+                table: "users",
+                column: "UserName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_users_UserName",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "users");
+        }
+    }
+}

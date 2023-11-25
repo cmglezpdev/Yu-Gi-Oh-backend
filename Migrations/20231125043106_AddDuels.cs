aba7cb8204724f084acd0405ea89e6136a68e579
Carlos Manuel González <cmglezpdev@gmail.com>
2023-11-24 23:35:34 -0500

    chore(database)[migrations]: add duels table

---
+++ Migrations/20231125043106_AddDuels.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDuels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "duels",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PlayerAId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PlayerBId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PlayerAScore = table.Column<int>(type: "integer", nullable: false),
+                    PlayerBScore = table.Column<int>(type: "integer", nullable: false),
+                    PlayerWinner = table.Column<Guid>(type: "uuid", nullable: true),
+                    TournamentId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Round = table.Column<int>(type: "integer", nullable: false),
+                    DuelNumber = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "NOW()"),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "NOW()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_duels", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "duels");
+        }
+    }
+}
